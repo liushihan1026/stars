b5d2967afe8707ccc7eff0f55698015d5799f733
Jeremy <liushihan1026@gmail.com>
2017-08-14 01:59:41 +0800

    整合Swagger、通用Mapper、JPA、Activiti6

--- src/main/java/com/it/Application.java
+++ src/main/java/com/it/Application.java
@@ -3,8 +3,12 @@ package com.it;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
+
+
+@ServletComponentScan
 @SpringBootApplication
 @EnableSwagger2
 @MapperScan("com.it.dao")
